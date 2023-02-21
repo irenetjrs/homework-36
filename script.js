d0caa2e51ba8f4a5c12cb1bc46e79fb21d53e80a
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-21 13:35:54 +0200

    task 2

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ let usersNames = ['Mike', 'Bob', 'Nikola'];
 let users = {};
 {users.mike = 'Mike', users.bob = 'Bob', users.nikola = 'Nikola'};
 console.log(users);
+
+// task 2
+
+let salaries = {
+   "Mike": 1500,
+   "Bob": 1800,
+   "Nikola": 1700
+};
+// console.log(Object.entries(salaries))
+function maxSalary(salaries) {
+   let maxNumber = 0;
+   let maxName = null;
+   for(const [key, value] of Object.entries(salaries)){
+      if (maxNumber < value){
+         maxNumber = value;
+         maxName = key;
+      }
+   }
+return maxName;
+}
+console.log(maxSalary(salaries));
